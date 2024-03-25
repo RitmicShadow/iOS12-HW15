116e2a249e2632ea554f9a5ede16ec71d78e06c3
DenisMayshev <denmay13@mail.ru>
2024-03-26 02:20:49 +0400

    Delete all coments

---
+++ iOS12-HW15/App/AppDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  AppDelegate.swift
+//  iOS12-HW15
+//
+//  Created by Ден Майшев on 26.03.2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate { }
---
+++ iOS12-HW15/App/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  iOS12-HW15
+//
+//  Created by Ден Майшев on 26.03.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let viewController = ViewController()
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+    }
+}
